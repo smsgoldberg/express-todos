9504a18993c83b18681da858ec92a007b1ff26f8
Sarah <sarahspagnolagoldberg@gmail.com>
2023-08-16 14:46:31 -0400

    middleware addition and explanation

--- server.js
+++ server.js
@@ -13,10 +13,29 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//Mount middle ware into the middleware/request pipeline
+//app.use([starts with path], <middleware fn> [, <middleware fn>])
+
+// add middleware below the above line of code
+app.use(function(req, res, next) {
+  console.log('Hello SEI!');
+  // Add a time property to the res.locals object
+  // The time property will then be accessible when rendering a view
+  res.locals.time = new Date().toLocaleTimeString();
+  next();  // Pass the request to the next middleware
+});
+
+//log in the terminal the HTTP request info
 app.use(logger('dev'));
+//processes data sent in the body of the request if it's json data
 app.use(express.json());
+//Processes data sent in 'form' body of the request
+//creates a property on req.body for each <input>, <select> or <textarea>
+//in the <form>
 app.use(express.urlencoded({ extended: false }));
+//Add a cookies property for each cookie sent in the request 
 app.use(cookieParser());
+//If the request is for a static asset, returns the file
 app.use(express.static(path.join(__dirname, 'public')));
 
 //The first arg is the "starts with" path
